79bf378537188ba95df8dc56cd7df163f0bca495
rishikts <59647688+rishikts@users.noreply.github.com>
2021-02-07 18:14:51 +0530

    Add files via upload

---
+++ Team5_ParaPhobic_Text Summariser/Scrap Code/berk.py
@@ -0,0 +1,6 @@
+pip install bert-extractive-summarizer
+pip install spacy==2.1.3
+pip install transformers==2.2.2
+pip install neuralcoref
+python -m spacy download en_core_web_md
+pip install rapydscript
